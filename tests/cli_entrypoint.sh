381b63d31f04471e2db73d6de78c4b8f208eb24c
Dushnilin <dushnilin@gmail.com>
2026-09-06 09:41:12 +0300

    test(cli): перенос теста вызова validate_byedpi_strategy_json в cli_entrypoint

--- tests/cli_entrypoint.sh
+++ tests/cli_entrypoint.sh
@@ -64,6 +64,10 @@ show_version_out="$(TACHYON_LIB="$fake_lib" ucode "$TACHYON_BIN" show_version)"
 [ "$show_version_out" = $'diagnostics\tshow-version\t\t\t' ] ||
   fail "show_version must dispatch through diagnostics/runtime.uc"
 
+byedpi_val_out="$(TACHYON_LIB="$fake_lib" ucode "$TACHYON_BIN" validate_byedpi_strategy_json '--disorder 3')"
+[ "$byedpi_val_out" = $'diagnostics\tvalidate-byedpi-strategy-json\t--disorder 3\t\t' ] ||
+  fail "validate_byedpi_strategy_json must dispatch through diagnostics/runtime.uc with arguments"
+
 subscription_out="$(TACHYON_LIB="$fake_lib" ucode "$TACHYON_BIN" subscription_update proxy 2)"
 [ "$subscription_out" = $'updates\tsubscription-update\tproxy\t2' ] ||
   fail "subscription_update must dispatch through components/updates.uc with arguments"
